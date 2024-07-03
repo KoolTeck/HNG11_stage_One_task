b7cbb920a171ef43567676c842c332c91ab5099a
Adetunji Adebowale <bowaleadetunji@gmail.com>
2024-07-03 12:30:50 +0100

    updates

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const app = express();
 const axios = require("axios");
 
 app.set("trust proxy", true);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const proxyDepth = parseInt(process.env.ADAPTABLE_TRUST_PROXY_DEPTH, 10);
 if (proxyDepth > 0) {
-  // 'trust proxy' is the number of IP addresses to trust in the
-  // X-Forwarded-For header, so set to the number of proxies plus one for the
-  // client IP address.
   app.set("trust proxy", proxyDepth + 1);
 }
 function getClientIp(req) {
